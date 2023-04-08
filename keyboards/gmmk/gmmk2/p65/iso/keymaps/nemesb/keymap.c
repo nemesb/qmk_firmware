cc9ab7eba505aa49234c118322e443b22d028045
Balázs Nemes <17362747+nemesb@users.noreply.github.com>
2023-04-08 13:17:26 +0200

    configure LEDs, add missing fn + esc

--- keyboards/gmmk/gmmk2/p65/iso/keymaps/nemesb/keymap.c
+++ keyboards/gmmk/gmmk2/p65/iso/keymaps/nemesb/keymap.c
@@ -28,8 +28,8 @@ enum {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: Base Layer (Default Layer)
-   */
+    /* Keymap _BL: Base Layer (Default Layer)
+     */
 [_BL] = LAYOUT(
     TD(TD_GRV_ESC), KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,
     KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_ENT,   KC_DEL,
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap _FL: Function Layer
      */
 [_FL] = LAYOUT(
-    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,  _______,
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
     _______,  _______,  RGB_SAI,  RGB_SAD,  RGB_TOG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,             KC_HOME,
     _______,  _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,   RGB_VAI,  KC_END,
@@ -81,4 +81,40 @@ bool caps_word_press_user(uint16_t keycode) {
         default:
             return false;  // Deactivate Caps Word.
     }
+}
+
+/* 
+ * Handle LEDs
+ */
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    // CAPS LOCK: red indicator
+    if (host_keyboard_led_state().caps_lock) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            if (g_led_config.flags[i] & LED_FLAG_UNDERGLOW) {
+                rgb_matrix_set_color(i, RGB_RED);
+            }
+        }
+        rgb_matrix_set_color(30, RGB_RED); //caps lock
+        rgb_matrix_set_color(44, RGB_RED); //l shift
+    }
+    return false;
+}
+
+void housekeeping_task_user(void) {
+    if (is_caps_word_on())
+    {
+        rgb_matrix_set_color_all(RGB_ORANGE);
+    }
+    
+    // Disable some LEDs
+    rgb_matrix_set_color(0,  RGB_OFF); // esc
+    rgb_matrix_set_color(13, RGB_OFF); // backspace
+    rgb_matrix_set_color(28, RGB_OFF); // enter
+    rgb_matrix_set_color(60, RGB_OFF); // win l
+    rgb_matrix_set_color(61, RGB_OFF); // alt l
+    rgb_matrix_set_color(62, RGB_OFF); // space
+    rgb_matrix_set_color(63, RGB_OFF); // alt r
+    rgb_matrix_set_color(64, RGB_OFF); // fn
+
 }
