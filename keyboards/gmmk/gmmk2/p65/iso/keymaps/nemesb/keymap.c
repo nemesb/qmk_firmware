cd96038d57e3bb5f619c82a54e77c64e3a1d8ff0
Balázs Nemes <17362747+nemesb@users.noreply.github.com>
2025-01-25 11:16:59 +0100

    add missing rgb control keys

--- keyboards/gmmk/gmmk2/p65/iso/keymaps/nemesb/keymap.c
+++ keyboards/gmmk/gmmk2/p65/iso/keymaps/nemesb/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FL] = LAYOUT_65_iso_blocker(
   KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,             _______,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_PGUP,
-  _______,  _______,  RM_HUEU,  RM_HUED,  RM_SPDD,  RM_SPDU,  KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,   RM_VALU,  KC_PGDN,
+  _______,  _______,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_PGUP,
+  _______,  _______,  RM_HUED,  RM_SATD,  RM_SPDD,  RM_TOGG,  KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,   RM_VALU,  KC_PGDN,
   _______,  _______,  _______,                                QK_BOOT,                                _______,  _______,  RM_PREV,   RM_VALD,  RM_NEXT) 
 };
 
